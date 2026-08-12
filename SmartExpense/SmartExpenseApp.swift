695f9cc509eb85c64d7f0b92dec400b874f956a0
saranyaJayakumar <saru.js111@gmail.com>
2026-08-12 14:01:14 +0530

    Initial Commit

---
+++ SmartExpense/SmartExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartExpenseApp.swift
+//  SmartExpense
+//
+//  Created by Saranya JayaKumar on 12/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SmartExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
